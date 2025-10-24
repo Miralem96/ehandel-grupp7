d93ea86cc00fb8a5c04b369b3456858a78ea841d
Linda Hansson <lhansson644@gmail.com>
2025-10-24 07:05:34 +0200

    added mean, std, color to bar

--- src/metrics.py
+++ src/metrics.py
@@ -8,7 +8,6 @@ df = pd.read_csv(CSV_PATH) # csv in to dataframe
 df.columns = df.columns.str.strip() # remove whitespaces
 
 def revenue_per_city(csv_path=CSV_PATH):
-    
     # group by city and sum revenue, sorted descending
     city_revenue = (
         df.groupby('city')['revenue']
@@ -18,25 +17,47 @@ def revenue_per_city(csv_path=CSV_PATH):
     return city_revenue, df
 
 if __name__ == "__main__":
-    
     # get revenue per city and full DataFrame
     city_rev, df = revenue_per_city()
     # convert to DataFrame for nicer display
     city_rev_df = city_rev.reset_index()
 
-    plt.bar(city_rev_df['city'], city_rev_df['revenue'])
-    plt.title('Vilken stad säljer mest?')
+     # Calculate mean and standard deviation of revenue
+    mean_revenue = df['revenue'].mean()
+    std_revenue = df['revenue'].std()
+
+    plt.bar(city_rev_df['city'], city_rev_df['revenue'], color='pink')
+
+
+    # Add text box inside the plot
+    plt.text(
+        0.95, 0.95,
+        f"Medelvärde: {mean_revenue:.2f}\nStd-avvikelse: {std_revenue:.2f}",
+        transform=plt.gca().transAxes,  # position relative to plot
+        ha='right', va='top',
+        fontsize=10,
+        bbox=dict(facecolor='white', alpha=0.8, edgecolor='gray')
+    )
+
+    plt.title('Revenue per city: ')
     plt.xlabel('city')
     plt.ylabel('revenue')
     plt.xticks(rotation=45, ha='right')  # så att stadnamnen syns bättre
     plt.tight_layout()  # fixar layouten
+    plt.legend()
     plt.show()
-    
-    # print results
-print("\nAlla städer med intäkt: ")
+
+   
+
+
+print("\nAlla städer och intäkt: ")
 print(city_rev_df)
-print(type(city_rev))  # <class 'pandas.core.series.Series'>
-print(type(df))        # <class 'pandas.core.frame.DataFrame'>
+print(f"\nMedelintäkt: {mean_revenue:.2f}")
+print(f"Standardavvikelse: {std_revenue:.2f}")   
+print(type(city_rev))  
+print(type(df))   
+
+  
 
 
 
