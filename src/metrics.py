e31a1011a4f1172f624895d583e6e1e1ebe35453
Linda Hansson <lhansson644@gmail.com>
2025-10-27 11:41:42 +0100

    added comments

--- src/metrics.py
+++ src/metrics.py
@@ -4,26 +4,26 @@ import matplotlib.pyplot as plt
 CSV_PATH = "/Users/lindahansson/Desktop/Gruppuppgift_1/data/ecommerce_sales.csv"
 
 df = pd.read_csv(CSV_PATH) 
-df.columns = df.columns.str.strip() 
+df.columns = df.columns.str.strip()  # remove whitepaces
 
-def revenue_per_city(csv_path=CSV_PATH):
+def revenue_per_city(csv_path=CSV_PATH):  # function to calculate total revenue per city
     city_revenue = (
-        df.groupby('city')['revenue']
-            .sum()
-            .sort_values(ascending=False)
+        df.groupby('city')['revenue']     # gorup data by city
+            .sum()                        # sum revenue values
+            .sort_values(ascending=False) # sort result decending
     )
     return city_revenue, df
 
 if __name__ == "__main__": 
-    city_rev, df = revenue_per_city() 
-    city_rev_df = city_rev.reset_index()
+    city_rev, df = revenue_per_city()     # Get revenue by city and DataFrame
+    city_rev_df = city_rev.reset_index()  # convert Series to df
 
-    mean_revenue = df['revenue'].mean()
-    std_revenue = df['revenue'].std()
+    mean_revenue = df['revenue'].mean()   # calculate mean revenue
+    std_revenue = df['revenue'].std()     # calculate standard deviation of revenue
 
-    fig, ax = plt.subplots()
-    ax.bar(city_rev_df['city'], city_rev_df['revenue'], color='pink',  width=0.5, edgecolor='black')
-    ax.text(
+    fig, ax = plt.subplots()              # create figure and axis for the bar chart
+    ax.bar(city_rev_df['city'], city_rev_df['revenue'], color='skyblue',  width=0.5, edgecolor='black')
+    ax.text(                              # text box showing mean and standard deviation
         0.95, 0.95,
         f"Medelvärde: {mean_revenue:.2f}\nStd-avvikelse: {std_revenue:.2f}",
         transform=ax.transAxes,
@@ -31,14 +31,19 @@ if __name__ == "__main__":
         fontsize=10,
         bbox=dict(facecolor='white', alpha=0.8, edgecolor='gray')
     )
+    # plot titles and axis labels
     ax.set_title('Revenue per city: ')
     ax.set_xlabel('city')
     ax.set_ylabel('revenue')
+
+    # set tick positions and rotate labels for readability
     ax.set_xticks(range(len(city_rev_df['city'])))
     ax.set_xticklabels(city_rev_df['city'], rotation=45, ha='right') 
-    fig.tight_layout()  
-    plt.show()
 
+    fig.tight_layout()                    # adjust layout to prevent label overlap
+    plt.show()                            # display the chart
+
+    # print summary statistics and data types
     print("\nAlla städer och intäkt: ")
     print(city_rev_df)
     print(f"\nMedelintäkt: {mean_revenue:.2f}")
